64001c7e1508931b2c003ad4b2cf7fb3eb586d1b
ManiTyagi <manityagi1919@gmail.com>
2025-07-11 01:04:44 +0530

    Backend commit

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -7,10 +7,10 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5173",
-        credentials: true,
-    },
+  cors: {
+    origin: ["http://localhost:5173", "*"],
+    credentials: true,
+  },
 });
 
 
